3b27869fa878635f7864a09ff66f9a210adfc75d
Hoang Huy <huyhoan@pdx.edu>
2016-05-15 22:54:17 +0700

    Edit some walkthrough

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,15 @@ class User < ActiveRecord::Base
     		user.name = auth.info.name   # assuming the user model has a name
     	
   		end
-	end
+	  end
 
-	def self.new_with_session(params, session)
-	    super.tap do |user|
-	      	if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-	        	user.email = data["email"] if user.email.blank?
-	      	end
-	    end
-	end
+  	def self.new_with_session(params, session)
+  	    super.tap do |user|
+  	      	if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+  	        	user.email = data["email"] if user.email.blank?
+  	      	end
+  	    end
+  	end
 
 
 
